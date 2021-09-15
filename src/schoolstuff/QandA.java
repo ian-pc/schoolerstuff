3c6bd05c778319f0749675e639d98a69e421e625
ian-pc <67145217+ian-pc@users.noreply.github.com>
2021-09-15 11:40:35 -0400

    Initial commit

---
+++ src/schoolstuff/QandA.java
@@ -0,0 +1,12 @@
+package schoolstuff;
+
+public class QandA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Q: How old are you? A: 14");
+		System.out.println("Q: Where do you live? A: Marr");
+		System.out.println("Q: What is your main language? A: C++");
+	}
+
+}
