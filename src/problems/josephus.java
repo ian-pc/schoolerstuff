a04369418f47b09cce7eabb02c9a7147e03001ca
ian-pc <67145217+ian-pc@users.noreply.github.com>
2021-12-07 21:01:22 -0500

    jhh

--- src/problems/josephus.java
+++ src/problems/josephus.java
@@ -5,43 +5,50 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 
+
+
 public class josephus {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
+	
+	public static void run(int j) {
+		System.out.println(j);
 		
-		
+		int size = j;
 		
 		ArrayList<Integer> al = new ArrayList<>();
 		for (int i = 0; i < size; i++) {
 			al.add(i);
 		}
 		
-		while(al.size() != 1) {
+		while(al.size() > 2) {
+		    int ogSize = al.size();
 		    int index = 1;
 		    Iterator<Integer> iterator = al.iterator();
-		    while(true) {
-		    	if (iterator.hasNext()) {
-			        iterator.next();
-			        if(index++ % 2 == 0) {
-			            iterator.remove();
-			        }
-		    	} else {
-		    		if (al.size()==2) {
-		    			al.remove(1);
-		    		} else {
-			    		al.remove(0);
-		    		}
-		    		break;
-		    	}
-		        
+		    while(iterator.hasNext()) {
+		        iterator.next();
+		        if(index++ % 2 == 0) {
+		            iterator.remove();
+		        }
 		    }
+		    if (ogSize % 2 == 1) {
+		    	al.remove(0);
+		    }
+			System.out.println(al);
 		} 
+		if (al.size() == 2) {
+			al.remove(1);
+		}
 
-	    System.out.println(al);
+		System.out.println(al);
+	    System.out.println("he" +  (al.get(0)+1));
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		run(sc.nextInt());
+		
+		
 		
 	}
 
